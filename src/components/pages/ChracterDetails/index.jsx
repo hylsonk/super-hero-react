2894ee44f525b31c3f3fe5dcce1b9bc088e9db07
Hylsonk <hylsonk@gmail.com>
2020-10-20 13:45:27 -0300

    exibindo loading de informações

--- src/components/pages/ChracterDetails/index.jsx
+++ src/components/pages/ChracterDetails/index.jsx
@@ -12,7 +12,7 @@ import {
 
 import api from '../../../services/api';
 
-export const ChracterDetails = ({match}) => {
+export const ChracterDetails = ({ match }) => {
 
     const [chracter, setChracter] = useState();
 
@@ -21,84 +21,45 @@ export const ChracterDetails = ({match}) => {
             const char = await get(match.params.id)
             setChracter(char.data)
         })()
-      }, []);
-
-    const mock = {
-        "id": "1",
-        "name": "A-Bomb",
-        "powerstats": {
-            "intelligence": "38",
-            "strength": "100",
-            "speed": "17",
-            "durability": "80",
-            "power": "24",
-            "combat": "64"
-        },
-        "biography": {
-            "full-name": "Richard Milhouse Jones",
-            "alter-egos": "No alter egos found.",
-            "aliases": [
-                "Rick Jones"
-            ],
-            "place-of-birth": "Scarsdale, Arizona",
-            "first-appearance": "Hulk Vol 2 #2 (April, 2008) (as A-Bomb)",
-            "publisher": "Marvel Comics",
-            "alignment": "good"
-        },
-        "appearance": {
-            "gender": "Male",
-            "race": "Human",
-            "height": [
-                "6'8",
-                "203 cm"
-            ],
-            "weight": [
-                "980 lb",
-                "441 kg"
-            ],
-            "eye-color": "Yellow",
-            "hair-color": "No Hair"
-        },
-        "work": {
-            "occupation": "Musician, adventurer, author; formerly talk show host",
-            "base": "-"
-        },
-        "connections": {
-            "group-affiliation": "Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom",
-            "relatives": "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
-        },
-        "image": {
-            "url": "https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg"
-        }
-    }
-
-    console.log(chracter)
+    }, []);
 
     return (
         <div className='container'>
-            <Image
-                url={mock.image.url}
-            />
-            <Powerstats 
-                powerstats={mock.powerstats}
-            />
-            <Biography 
-                biography={mock.biography}
-            />
-            <Appearance 
-                appearance={mock.appearance}
-            />
-            <Work 
-                work={mock.work}
-            />
-            <Connections 
-                connections={mock.connections}
-            />
+            {View(chracter)}
         </div>
     )
 }
 
-export const get = async (i) =>{
+export const View = (chracter) => {
+    if (chracter) {
+        return (
+            <div>
+                <Image
+                    url={chracter.image.url}
+                />
+                <Powerstats
+                    powerstats={chracter.powerstats}
+                />
+                <Biography
+                    biography={chracter.biography}
+                />
+                <Appearance
+                    appearance={chracter.appearance}
+                />
+                <Work
+                    work={chracter.work}
+                />
+                <Connections
+                    connections={chracter.connections}
+                />
+            </div>
+        )
+    } else {
+        return ("Loading...")
+    }
+}
+
+export const get = async (i) => {
     const result = await api.get(`${i}`);
     return result;
 }
